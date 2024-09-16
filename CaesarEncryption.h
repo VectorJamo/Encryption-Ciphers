ca6003dd20b39b73276ec051e4bd661dca0a2439
Suraj Neupane <71274266+VectorJamo@users.noreply.github.com>
2024-09-16 09:00:15 -0600

    Update CaesarEncryption.h

--- CaesarEncryption.h
+++ CaesarEncryption.h
@@ -5,10 +5,8 @@ struct EncryptionStatus {
 };
 
 int getEncryptedChar(char c, int keyOffset);
+int getDecryptedChar(char c, int keyOffset);
 
 // For encryption and decryption by giving a key beforehand
 EncryptionStatus Encrypt(const char* filepath, int keyOffset);
-EncryptionStatus Decrypt(const char* filepath, int keyOffset);
-
-// For encryption using a random key offset value, returns the 
-EncryptionStatus Encrypt(const char* filepath);
+bool Decrypt(const char* filepath, int keyOffset);
